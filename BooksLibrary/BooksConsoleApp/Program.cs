145cdffbca475d2d456f757ce70dbf890334c6d3
nkitsan <nastik.kitsan@gmail.com>
2019-04-08 01:16:15 +0300

    Add class to store collections in files

--- BooksLibrary/BooksConsoleApp/Program.cs
+++ BooksLibrary/BooksConsoleApp/Program.cs
@@ -8,19 +8,14 @@ namespace BooksConsoleApp
     {
         static void Main(string[] args)
         {
-            var books = new BookCollection(
-                new List<Book>
-                {
-                    new Book("9781107699892", "Murphy", "Advanced Grammar in Use Book", "Cambridge University Press", 2013, 260, 65.0M),
-                    new Book("9785170996261", "King", "Pet Sematary", "AST", 2016, 480, 8.0M)
-                });
-
+            var bookListStorage = new BookListStorage("test.txt");
+            var books = bookListStorage.LoadDataFromFile();
             var bookListService = new BookListService(books);
-            var orederedByAuthor = bookListService.SortBooksByAuthor();
 
-            foreach (var b in orederedByAuthor)
+            foreach (var book in bookListService.SortBooksByAuthor())
             {
-                Console.WriteLine(b.ToString());
+                Console.WriteLine(book);
+                Console.WriteLine("--------------------------------");
             }
 
             Console.ReadKey();
--- BooksLibrary/BooksLibrary/BookListStorage.cs
+++ BooksLibrary/BooksLibrary/BookListStorage.cs
@@ -1,10 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace BooksLibrary
 {
     public class BookListStorage
     {
+        private string fileName;
+
+        public BookListStorage(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public void SaveDataToFile(BookCollection books)
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open(fileName, FileMode.Create)))
+            {
+                foreach (var book in books)
+                {
+                    writer.Write(book.ISBN);
+                    writer.Write(book.Author);
+                    writer.Write(book.Name);
+                    writer.Write(book.Publisher);
+                    writer.Write(book.Year);
+                    writer.Write(book.Pages);
+                    writer.Write(book.Price);
+                }
+            }
+        }
+
+        public BookCollection LoadDataFromFile()
+        {
+            var result = new List<Book>();
+
+            if (File.Exists(this.fileName))
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(this.fileName, FileMode.Open)))
+                {
+                    while (reader.PeekChar() > -1)
+                    {
+                        result.Add(new Book(
+                            reader.ReadString(),
+                            reader.ReadString(),
+                            reader.ReadString(),
+                            reader.ReadString(),
+                            reader.ReadInt16(),
+                            reader.ReadInt16(),
+                            reader.ReadDecimal()
+                        ));
+                    }
+                }
+            }
+
+            return new BookCollection(result);
+        }
     }
 }
